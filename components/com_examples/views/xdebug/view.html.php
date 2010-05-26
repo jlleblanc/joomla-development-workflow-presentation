c6d4016b7995ac7b4e2eb6dc98536d7485e7f0cb
Joe LeBlanc <contact@jlleblanc.com>
2010-05-26 10:50:58 -0400

    Moved the stack trace to a place where it can do some good.

--- components/com_examples/views/xdebug/view.html.php
+++ components/com_examples/views/xdebug/view.html.php
@@ -1,19 +1,19 @@
 <?php
 defined( '_JEXEC' ) or die;
 
-xdebug_start_trace(JPATH_COMPONENT . '/views/xdebug/function_trace');
-
 jimport( 'joomla.application.component.view');
 
 class ExamplesViewXdebug extends JView
 {
 	public function display($tpl = null)
 	{
+		xdebug_start_trace(JPATH_COMPONENT . '/views/xdebug/function_trace');
+
 		$data = $this->get('Data');
 		$this->assign('data', $data);
 
 		parent::display($tpl);
+
+		xdebug_stop_trace();
 	}
 }
-
-xdebug_stop_trace();
